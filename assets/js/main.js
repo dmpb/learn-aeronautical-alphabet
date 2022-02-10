e3f56abd07360757498d2612a52d777327e8fbcb
Javier Vargas <jabo94454@gmail.com>
2022-02-10 14:18:09 -0500

    Se ha agregado al final una tabla indicando los errores, además se solucionó algunos bugs. Faltaría que nos muestre las letras en desorden.

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,23 +12,49 @@ let indexAlphabet = 1;
 let formulario = document.getElementById('form');
 let hour = document.getElementById('hour');
 let input = document.getElementById('inputValueUser');
+let paragraph = document.getElementById('paragraph');
+let results = document.getElementById('content__results');
+let listWrong = document.getElementById('listWrong');
 let answerCorrect = 0,
     answerWrong = 0,
-    futureHour, idInterval;
+    futureHour, idInterval, arrayWrong = [],
+    arrayCorrect = [],
+    validateTime = '';
 
 // Capturamos el evento cada que se envía una palabra en el input, para que valide si la respuesta es correcta y pase a la siguiente letra.
 form.addEventListener('submit', (event) => {
     event.preventDefault();
     let form = new FormData(event.target);
     let inputValue = form.get('inputValueUser');
-    (inputValue.toLowerCase() === alphabetAeronatic[indexAlphabet - 1]) ? answerCorrect++ : answerWrong++;
+    if (inputValue.toLowerCase() === alphabetAeronatic[indexAlphabet - 1]) {
+        answerCorrect++
+    } else {
+        answerWrong++;
+        addTable(alphabetAeronatic[indexAlphabet - 1], inputValue.toLowerCase());
+    }
     clearInput();
     changeLetter();
 });
 
+function addTable(wordCorrect, wordWrong) {
+    let listGroup = document.createElement('tr');
+    let childrenListCorrect = document.createElement('td');
+    let childrenListWrong = document.createElement('td');
+    let childrenWordCorrect = document.createTextNode(wordCorrect);
+    let childrenWordWrong = document.createTextNode(wordWrong);
+    childrenListCorrect.appendChild(childrenWordCorrect);
+    childrenListWrong.appendChild(childrenWordWrong);
+    childrenListWrong.className = 'error';
+    listGroup.append(childrenListCorrect);
+    listGroup.append(childrenListWrong);
+    listWrong.append(listGroup);
+    return listWrong;
+}
+
 // Cada que le del click al input, el temporizador inicia
 input.addEventListener('focus', () => {
-    startTimer(1, 0);
+    if (validateTime) { return }
+    startTimer(0, 2);
 });
 
 document.getElementById('resetGame').addEventListener('click', () => {
@@ -46,25 +72,37 @@ function changeLetter() {
     indexAlphabet++;
     if (indexAlphabet === 27) {
         clearInterval(idInterval);
-        console.log('Hemos terminado');
-        document.getElementById('letter').innerHTML = '¡Terminó el tiempo!'
         showResults();
     }
 }
 
 // Mostramos los resultados una vez termine el temporizador o se terminen las letras
 function showResults() {
+    validateTime = '';
     let answer__correct = document.getElementById('answer__correct');
     let answer__wrong = document.getElementById('answer__wrong');
     answer__correct.innerHTML = answerCorrect;
     answer__wrong.innerHTML = answerWrong;
-    let results = document.getElementById('content__results');
-    console.log('estoy aquí');
-    formulario.style.display = "none";
-    results.style.display = "block";
+    document.getElementById('letter').innerHTML = '¡Terminó el tiempo!'
+    if (answerWrong == 0) hideElement(document.getElementById('table'));
+    let trList = document.createElement('tr');
+
+    // for (let i = 0; i < answerWrong; i++) {
+    //     let list = document.createElement('td');
+    //     let text = document.createTextNode(`${arrayCorrect[i]} - ${arrayWrong[i]}`);
+    //     list.append(text);
+    //     trList.append(list);
+    // }
+
+    hideElement(formulario);
+    hideElement(paragraph);
+    hideElement(hour);
+
+    showElement(results);
 }
 
 const startTimer = (minutes, seconds) => {
+    validateTime = ' ';
     if (futureHour) {
         futureHour = new Date(new Date().getTime());
     } else {
@@ -93,9 +131,18 @@ const formatHourZero = valor => {
         return "" + valor;
     }
 }
+
 const milisecondsAminutesYseconds = (miliseconds) => {
     const minutes = parseInt(miliseconds / 1000 / 60);
     miliseconds -= minutes * 60 * 1000;
     seconds = (miliseconds / 1000);
     return `${formatHourZero(minutes)}:${formatHourZero(seconds.toFixed(1))}`;
 };
+
+const hideElement = (element) => {
+    element.style.display = 'none';
+}
+
+const showElement = (element) => {
+    element.style.display = 'block';
+}
