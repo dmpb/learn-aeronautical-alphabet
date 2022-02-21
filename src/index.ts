3f8ede5bd5d1560e004d8a431433863f493c894b
Daniel Ponce <dmep17@gmail.com>
2022-02-21 00:34:22 -0500

    Agregar interación para el usuario

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,47 @@
 import "./assets/css/main.css";
 import Aeronoutical from "./libs/aeronautical";
 
-const play = <HTMLElement>document.getElementById("play");
+// Obtener los elemntos HTML
+const play = <HTMLButtonElement>document.getElementById("play");
+const letter = <HTMLElement>document.getElementById("letter");
 const input = <HTMLInputElement>document.getElementById("input");
-const next = <HTMLElement>document.getElementById("next");
+const next = <HTMLButtonElement>document.getElementById("next");
+const success = <HTMLElement>document.getElementById("success");
+const error = <HTMLElement>document.getElementById("error");
+const resultContainer = <HTMLElement>document.getElementById("result");
 
+// Establecer la instancia
 const aeronautical = new Aeronoutical({
-  time: 60000,
+  // Establece el tiempo de intervalo del juego
+  time: 60000, // 1 minuto
+  // Callback que se ejecuta cuando el juego a finalizado
   finished: (result) => {
     console.log("finished");
-    console.log(result);
+    success.innerHTML = result.success.toString();
+    error.innerHTML = result.error.toString();
+    resultContainer.style.display = "block";
+    play.disabled = false;
+    input.disabled = true;
+    next.disabled = true;
   },
 });
 
+// Evento para iniciar el juego
 play.addEventListener("click", () => {
   const alphabet = aeronautical.play();
 
-  console.log(alphabet);
+  letter.innerHTML = alphabet.symbol;
+  resultContainer.style.display = "none";
+  play.disabled = true;
+  input.disabled = false;
+  next.disabled = false;
 });
 
+// Evento para pasar al siguiente letra
 next.addEventListener("click", () => {
   const value = input.value;
-
   const alphabet = aeronautical.next(value);
 
-  console.log(alphabet);
+  input.value = "";
+  letter.innerHTML = alphabet.symbol;
 });
